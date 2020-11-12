0c1e31428420697a3fb406dd808221779902f934
Andre R. Ferreira <deveras@users.noreply.github.com>
2020-11-12 12:39:40 +0000

    Add TutorialModel and TutorialAdapter

---
+++ src/app/models/tutorial-model.spec.ts
@@ -0,0 +1,113 @@
+import { TutorialModel, TutorialAdapter } from './tutorial-model';
+
+
+describe('TutorialModel', () => {
+  let subjectUnderTest:TutorialModel;
+  let dateInstance = new Date();
+
+
+  beforeEach( () => {
+    subjectUnderTest = new TutorialModel();
+    subjectUnderTest.lastUpdateDate = dateInstance;
+    subjectUnderTest.createDate = dateInstance;
+  });
+
+
+  it('should create an instance', () => {
+    expect( subjectUnderTest ).toBeTruthy();
+  });
+
+
+  it('should have exactly 6 properties', () => {
+    expect( Object.getOwnPropertyNames(subjectUnderTest).length ).toBe(6);
+  });
+
+
+ it('should have defined the following properties and types', () => {
+    expect( subjectUnderTest.id ).toBeDefined();
+    expect( subjectUnderTest.id ).toBeInstanceOf(Number);
+    expect( subjectUnderTest.title ).toBeDefined();
+    expect( subjectUnderTest.title ).toBeInstanceOf(String);
+    expect( subjectUnderTest.recomendationSummary ).toBeDefined();
+    expect( subjectUnderTest.recomendationSummary ).toBeInstanceOf(String);
+    expect( subjectUnderTest.body ).toBeDefined();
+    expect( subjectUnderTest.body ).toBeInstanceOf(String);
+    expect( subjectUnderTest.lastUpdateDate ).toBeDefined();
+    expect( subjectUnderTest.lastUpdateDate ).toBeInstanceOf(Date);
+    expect( subjectUnderTest.createDate ).toBeDefined();
+    expect( subjectUnderTest.createDate ).toBeInstanceOf(Date);
+  });
+
+
+ it('should have the following properties defaults', () => {
+    expect( subjectUnderTest.id ).toBe(0);
+    expect( subjectUnderTest.title ).toBe("");
+    expect( subjectUnderTest.recomendationSummary ).toBe("");
+    expect( subjectUnderTest.body ).toBe("");
+    expect( subjectUnderTest.lastUpdateDate ).toEqual(dateInstance);
+    expect( subjectUnderTest.createDate ).toEqual(dateInstance);
+  });
+
+
+  it('should allow public update of all its properties values', () => {
+    subjectUnderTest.id = 100;
+    subjectUnderTest.title = "foo";
+    subjectUnderTest.recomendationSummary = "bar";
+    subjectUnderTest.body = "foobaz";
+    subjectUnderTest.lastUpdateDate = dateInstance;
+    subjectUnderTest.createDate = dateInstance;
+
+    expect( subjectUnderTest.id ).toBe(100);
+    expect( subjectUnderTest.title ).toBe("foo");
+    expect( subjectUnderTest.recomendationSummary ).toBe("bar");
+    expect( subjectUnderTest.body ).toBe("foobaz");
+    expect( subjectUnderTest.lastUpdateDate ).toEqual(dateInstance);
+    expect( subjectUnderTest.createDate ).toEqual(dateInstance);
+  });
+});
+
+
+
+describe('TutorialAdapter', () => {
+  let subjectUnderTest:TutorialAdapter;
+  let dateInstance = new Date();
+
+
+  beforeEach( () => {
+    subjectUnderTest = new TutorialAdapter();
+  });
+
+
+  it('should create an instance', () => {
+    expect( subjectUnderTest ).toBeTruthy();
+  });
+
+
+  it('should have an adapt method', () => {
+    expect( subjectUnderTest.adapt ).toBeDefined();
+    expect( typeof subjectUnderTest.adapt ).toBe("function");
+  });
+
+
+  it('adapt should return a TutorialAdapter instance', () => {
+    const mockDateString = "1977-11-19 03:00:00";
+    const mockResponse = {
+      id: 100,
+      title: "foo",
+      recomendationSummary: "bar",
+      body: "baz",
+      lastUpdate: mockDateString,
+      createDate: mockDateString,
+    };
+
+    const tutorialModel:TutorialModel = subjectUnderTest.adapt(mockResponse);
+
+    expect( tutorialModel.id ).toBe(100);
+    expect( tutorialModel.title ).toBe("foo");
+    expect( tutorialModel.recomendationSummary ).toBe("bar");
+    expect( tutorialModel.body ).toBe("baz");
+    expect( tutorialModel.lastUpdateDate ).toEqual(new Date(mockDateString));
+    expect( tutorialModel.createDate ).toEqual(new Date(mockDateString));
+  });
+
+});
---
+++ src/app/models/tutorial-model.ts
@@ -0,0 +1,45 @@
+import { Injectable } from "@angular/core";
+import { Adapter } from "./adapter";
+
+
+export class TutorialModel
+{
+  constructor(
+    public id:number = 0,
+    public title:string = "",
+    public recomendationSummary:string = "",
+    public body:string = "",
+    public lastUpdateDate:Date = new Date(),
+    public createDate:Date = new Date()
+  ) {
+    this.id = id;
+    this.title = title;
+    this.recomendationSummary = recomendationSummary;
+    this.body = body;
+    this.lastUpdateDate = lastUpdateDate;
+    this.createDate = createDate;
+  }
+
+}
+
+
+
+@Injectable(
+  {
+    providedIn: "root"
+  }
+)
+export class TutorialAdapter
+  implements Adapter<TutorialModel>
+{
+  adapt(item:any):TutorialModel {
+    return new TutorialModel(
+      item.id,
+      item.title,
+      item.recomendationSummary,
+      item.body,
+      new Date(item.lastUpdate),
+      new Date(item.createDate)
+    )
+  }
+}
