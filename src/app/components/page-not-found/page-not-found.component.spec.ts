22671745676368a781533a62d6027ffa4424ff91
Andre R. Ferreira <deveras@users.noreply.github.com>
2020-11-13 09:03:04 +0000

    Add TitleService to PageNotFoundComponent

--- src/app/components/page-not-found/page-not-found.component.spec.ts
+++ src/app/components/page-not-found/page-not-found.component.spec.ts
@@ -1,15 +1,19 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { Title } from '@angular/platform-browser';
 import { PageNotFoundComponent } from './page-not-found.component';
 
 
 describe('PageNotFoundComponent', () => {
   let fixture: ComponentFixture<PageNotFoundComponent>;
   let component: PageNotFoundComponent;
+  let titleService:Title;
+  let spyTitleServiceSet:jasmine.Spy;
 
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ PageNotFoundComponent ]
+      declarations: [ PageNotFoundComponent ],
+      providers: [ Title ]
     })
   });
 
@@ -17,12 +21,22 @@ describe('PageNotFoundComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(PageNotFoundComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+
+    titleService = TestBed.inject(Title);
+    spyTitleServiceSet = spyOn(titleService, "setTitle");
   });
 
 
   it('should create the component', () => {
     expect( component ).toBeTruthy();
+  });
+
+
+  it('ngOnInit should set the page title to Bookshelf', () => {
+    fixture.detectChanges();
+
+    expect( spyTitleServiceSet.calls.count() ).toBe(1);
+    expect( spyTitleServiceSet ).toHaveBeenCalledWith("Page not found!");
   });
 
 });
--- src/app/components/page-not-found/page-not-found.component.ts
+++ src/app/components/page-not-found/page-not-found.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 
-@Component({
-  selector: 'blog-page-not-found',
-  templateUrl: './page-not-found.component.html',
-  styleUrls: ['./page-not-found.component.scss']
-})
-export class PageNotFoundComponent implements OnInit {
 
-  constructor() { }
+@Component(
+  {
+    selector: 'blog-page-not-found',
+    templateUrl: './page-not-found.component.html',
+    styleUrls: ['./page-not-found.component.scss']
+  }
+)
+export class PageNotFoundComponent
+  implements OnInit
+{
 
-  ngOnInit(): void {
+  constructor(private titleService:Title) { }
+
+
+  ngOnInit():void {
+    this.titleService.setTitle("Page not found!");
   }
 
 }
